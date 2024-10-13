21ddf6e7fd0d348e8700b5f67b294f799f74d805
Gavin Xu <g58xu@uwaterloo.ca>
2024-10-13 13:40:50 -0400

    Refactor to ES Modules

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+// Imports
+import fs from 'node:fs';
+import { Client, Events, GatewayIntentBits } from 'discord.js';
 
+// Constants
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+const { token } = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
 
+// Main Script
 client.once(Events.ClientReady, readyClient => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
