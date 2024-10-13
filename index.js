76ba7b0ceadfef4366072e952236da36f46b496f
Gavin Xu <g58xu@uwaterloo.ca>
2024-10-13 14:00:39 -0400

    Implement command loader

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // Imports
 import fs from 'node:fs';
 import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { loadCmds } from './commands/cmdLoader.js';
 
 // Constants
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 const { token } = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
 
 // Main Script
+loadCmds(client);
+
 client.once(Events.ClientReady, readyClient => {
     console.log(`[LOG] Ready! Logged in as ${readyClient.user.tag}`);
 });
