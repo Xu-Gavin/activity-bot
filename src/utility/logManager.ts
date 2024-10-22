6bdcacecdb1dee7eb3a155808165bc61a205fd3d
Gavin Xu <g58xu@uwaterloo.ca>
2024-10-21 22:10:29 -0400

    Add template LogManager

---
+++ src/utility/logManager.ts
@@ -0,0 +1,13 @@
+class LogManager {
+    static logInfo(msg: string) {
+        console.log(); // TODO: finish
+    }
+
+    static logWarning(msg: string) {
+        console.log(); // TODO: finish
+    }
+
+    static logError(msg: string) {
+        console.log(); // TODO: finish
+    }
+}
