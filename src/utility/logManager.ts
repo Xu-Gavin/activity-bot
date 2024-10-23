4db40e15f22184eaee8484a244fcf85d81f0b64f
Gavin Xu <g58xu@uwaterloo.ca>
2024-10-23 18:03:21 -0400

    Add constants

--- src/utility/logManager.ts
+++ src/utility/logManager.ts
@@ -1,5 +1,11 @@
 import chalk from "chalk";
 
+// Constants
+const error = chalk.bold.red;
+const warning = chalk.hex('#FFA500');
+const success = chalk.greenBright;
+const info = chalk.cyan;
+
 class LogManager {
     static logInfo(msg: string) {
         console.log(); // TODO: finish
